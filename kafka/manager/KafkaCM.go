3a6075d59ff1f0c639038119bc9296bae500b0f7
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-03-21 19:00:29 +0530

    api

---
+++ kafka/manager/KafkaCM.go
@@ -0,0 +1,13 @@
+package manager
+
+func StartConsumer(hostel string) {
+	kafkaBroker := "kafka-broker:9092"
+	var topic = []string{
+		"complaint_registration",
+	}
+
+	switch hostel {
+	case "BH1", "BH6":
+
+	}
+}
