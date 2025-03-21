33164729e2a1e1a590ab44a97a89a1a09d6d68fd
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-03-21 18:57:09 +0530

    api

--- kafka/manager/KafkaPM.go
+++ kafka/manager/KafkaPM.go
@@ -2,12 +2,14 @@ package manager
 
 import (
 	"fmt"
+	"log"
 
 	"github.com/adityjoshi/Dosahostel/kafka/producer"
 )
 
 type KafkaManager struct {
 	BigBoysHostel *producer.BigBoysHostel
+	BoysHostel    *producer.BoysHostel
 }
 
 func NewKafkaManager(BigBoysBroker, BoysBroker []string) (*KafkaManager, error) {
@@ -16,8 +18,38 @@ func NewKafkaManager(BigBoysBroker, BoysBroker []string) (*KafkaManager, error) 
 		return nil, fmt.Errorf("error initializing North producer: %w", err)
 	}
 
+	BoysHostel, err := producer.BoysProducer(BoysBroker)
+	if err != nil {
+		return nil, fmt.Errorf("error initializing North producer: %w", err)
+	}
+
 	// Return the KafkaManager instance with both producers
 	return &KafkaManager{
 		BigBoysHostel: BigBoysHostel,
+		BoysHostel:    BoysHostel,
 	}, nil
+}
+
+func (km *KafkaManager) ComplaintRegistration(hostel, topic, messageString string) error {
+	var err error
+
+	switch hostel {
+	case "BH1", "BH6":
+		log.Printf("Sending message to North region, topic: %s", topic)
+		err = km.BigBoysHostel.SendMessage(topic, messageString)
+	case "BH2", "BH3", "BH4", "BH5":
+
+		log.Printf("Sending message to South region, topic: %s", topic)
+		err = km.BoysHostel.SendMessageBoys(topic, messageString)
+	default:
+
+		return fmt.Errorf("invalid hostel: %s", hostel)
+	}
+	if err != nil {
+		return fmt.Errorf("failed to send message to Kafka topic %s in %s region: %w", topic, hostel, err)
+	}
+
+	// Log successful message sending
+	log.Printf("Message successfully sent to topic %s in %s region", topic, hostel)
+	return nil
 }
---
+++ kafka/producer/bh2_bh5_producer.go
@@ -0,0 +1,43 @@
+package producer
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/IBM/sarama"
+)
+
+type BoysHostel struct {
+	producer sarama.SyncProducer
+}
+
+// BigBoysProducer initializes and returns a new NorthProducer
+func BoysProducer(brokers []string) (*BoysHostel, error) {
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+	config.Producer.Return.Successes = true
+
+	producer, err := sarama.NewSyncProducer(brokers, config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to start North Kafka producer: %w", err)
+	}
+
+	return &BoysHostel{producer: producer}, nil
+}
+
+// SendMessage sends a message to a specific topic in the North region
+func (p *BoysHostel) SendMessageBoys(topic, message string) error {
+	log.Printf("Producer received message: %s", message)
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(message),
+	}
+	_, _, err := p.producer.SendMessage(msg)
+	if err != nil {
+		log.Printf("Failed to send message to topic %s: %v", topic, err)
+		return err
+	}
+	log.Printf("Message sent to topic %s in BH2-BH5 region", topic)
+	return nil
+}
