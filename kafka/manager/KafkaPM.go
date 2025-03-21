0a5703aa59068fa08398ad34b864a6764a0a0299
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-03-21 18:43:51 +0530

    api

--- kafka/manager/KafkaPM.go
+++ kafka/manager/KafkaPM.go
@@ -1,5 +1,23 @@
 package manager
 
+import (
+	"fmt"
+
+	"github.com/adityjoshi/Dosahostel/kafka/producer"
+)
+
 type KafkaManager struct {
-	BigBoysProducer
+	BigBoysHostel *producer.BigBoysHostel
+}
+
+func NewKafkaManager(BigBoysBroker, BoysBroker []string) (*KafkaManager, error) {
+	BigBoysHostel, err := producer.BigBoysProducer(BigBoysBroker)
+	if err != nil {
+		return nil, fmt.Errorf("error initializing North producer: %w", err)
+	}
+
+	// Return the KafkaManager instance with both producers
+	return &KafkaManager{
+		BigBoysHostel: BigBoysHostel,
+	}, nil
 }
