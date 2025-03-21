1f92775c3794103bb0361dadca7d8e25224dca37
Aditya Joshi <111140014+adityjoshi@users.noreply.github.com>
2025-03-21 18:35:03 +0530

    api

---
+++ kafka/manager/KafkaPM.go
@@ -0,0 +1,5 @@
+package manager
+
+type KafkaManager struct {
+	BigBoysProducer
+}
---
+++ kafka/producer/bh1_bh6_producer.go
@@ -0,0 +1,43 @@
+package producer
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/IBM/sarama"
+)
+
+type BigBoysHostel struct {
+	producer sarama.SyncProducer
+}
+
+// BigBoysProducer initializes and returns a new NorthProducer
+func BigBoysProducer(brokers []string) (*BigBoysHostel, error) {
+	config := sarama.NewConfig()
+	config.Producer.RequiredAcks = sarama.WaitForAll
+	config.Producer.Retry.Max = 5
+	config.Producer.Return.Successes = true
+
+	producer, err := sarama.NewSyncProducer(brokers, config)
+	if err != nil {
+		return nil, fmt.Errorf("failed to start North Kafka producer: %w", err)
+	}
+
+	return &BigBoysHostel{producer: producer}, nil
+}
+
+// SendMessage sends a message to a specific topic in the North region
+func (p *BigBoysHostel) SendMessage(topic, message string) error {
+	log.Printf("Producer received message: %s", message)
+	msg := &sarama.ProducerMessage{
+		Topic: topic,
+		Value: sarama.StringEncoder(message),
+	}
+	_, _, err := p.producer.SendMessage(msg)
+	if err != nil {
+		log.Printf("Failed to send message to topic %s: %v", topic, err)
+		return err
+	}
+	log.Printf("Message sent to topic %s in North region", topic)
+	return nil
+}
